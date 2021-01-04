15f29d7969d4963a32cb308731ff43fe7a7c3908
Eduardo <eduardorostran@yahoo.com>
2021-01-03 21:51:54 -0600

    trabajando en el mai

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+if __name__ == "__main__":
+    print("""
+    Acciones disponibles:
+        1 - Registro
+        2 - Login
+    """)
+
+    accion = int(input('que quieres hacer: '))
+
+    if accion == 1:
+        print("Ok, vamos a resitararnos en el sistemas")
+    elif accion == 2:
+        print("Ok, vamos a iniciar seccion")
+        
