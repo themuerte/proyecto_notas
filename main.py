abbbb2a57d115cb7acc9ef9fd8e623448d26ca3d
Eduardo <eduardorostran@yahoo.com>
2021-01-03 21:59:34 -0600

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 
-
-
-
-
 if __name__ == "__main__":
     print("""
     Acciones disponibles:
@@ -14,6 +10,12 @@ if __name__ == "__main__":
 
     if accion == 1:
         print("Ok, vamos a resitararnos en el sistemas")
+        nombre = input('cual es tu nombre?: ')
+        apellidos = input('cuales osn tus apellidos?: ')
+        email = input('cual es tu email?: ')
+        password = input('ingresa tu contraseña: ')
+
     elif accion == 2:
         print("Ok, vamos a iniciar seccion")
-        
+        email = input('cual es tu email?: ')
+        password = input('cual es tu contraseña: ')
