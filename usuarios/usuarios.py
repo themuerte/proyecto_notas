95515389c0e40c3453c4ddde19b6b88fc19641c5
Eduardo <eduardorostran@yahoo.com>
2021-01-04 23:21:27 -0600

    usuarios.py

---
+++ usuarios/usuarios.py
@@ -0,0 +1,15 @@
+class Usuarios:
+
+    def __init__(self, nombre, apellidos, email, password):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.email = email
+        self.password = password
+
+    def Registrar(self):
+        return self.nombre
+
+
+    def Identificar(self):
+        self.nombre
+
